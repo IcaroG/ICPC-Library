811dec8bce6ea603064e87b2d818ded46f712cc7
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-03-06 19:12:07 -0300

    Create Kruskal.cpp

---
+++ Kruskal.cpp
@@ -0,0 +1,15 @@
+vector<pair<int, ii>> edge;
+vector<int> g[ms];
+
+void kruskal(){
+	sort(edge.begin(), edge.end());
+
+	for(int i = m - 1; i >= 0; --i){
+		int w = edge[i].first, u = edge[i].second.first, v = edge[i].second.second;
+		if(dsFind(u) != dsFind(v)){
+			dsUnion(u, v);
+			g[u].push_back({v, w});
+			g[v].push_back({u, w});
+		}
+	}
+}
