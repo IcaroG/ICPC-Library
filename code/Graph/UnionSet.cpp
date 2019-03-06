af92a6068dd762c28d240d435df6b30c4d152ab9
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-03-06 19:18:18 -0300

    Create UnionSet.cpp

---
+++ code/Graph/UnionSet.cpp
@@ -0,0 +1,24 @@
+int ds[ms], sz[ms];
+
+void dsBuild(){
+	for(int i = 0; i<n; ++i){
+		ds[i] = i;
+		sz[i] = 1;
+	}
+}
+
+int dsFind(int i){
+	while(ds[i] != i){
+		ds[i] = ds[ds[i]];
+		i = ds[i];
+	}
+	return i;
+}
+
+void dsUnion(int a, int b){
+	a = dsFind(a);
+	b = dsFind(b);
+	if(sz[a] < sz[b]) swap(a, b);
+	sz[a] += sz[b];
+	ds[b] = a;
+}
