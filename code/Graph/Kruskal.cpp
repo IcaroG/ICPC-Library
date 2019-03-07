221ca5e0e080ff39039aba6968c4d508672de015
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-03-06 21:20:53 -0300

    Update Kruskal.cpp

--- code/Graph/Kruskal.cpp
+++ code/Graph/Kruskal.cpp
@@ -2,15 +2,14 @@ vector<pair<int, ii>> edge;
 vector<ii> g[ms];
 
 void kruskal(){
-	sort(edge.begin(), edge.end());
+    sort(edge.begin(), edge.end());
 
-	for(int i = m - 1; i >= 0; --i){
-		int w = edge[i].first, u = edge[i].second.first, v = edge[i].second.second;
-        a
-		if(dsFind(u) != dsFind(v)){
-			dsUnion(u, v);
-			g[u].push_back({v, w});
-			g[v].push_back({u, w});
-		}
-	}
+    for(int i = m - 1; i >= 0; --i){
+        int w = edge[i].first, u = edge[i].second.first, v = edge[i].second.second;
+        if(dsFind(u) != dsFind(v)){
+            dsUnion(u, v);
+            g[u].push_back({v, w});
+            g[v].push_back({u, w});
+        }
+    }
 }
