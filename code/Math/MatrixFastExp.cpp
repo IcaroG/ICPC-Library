c3a9e64e8c07a84504f0314c6ee4e0ccd8372162
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-03-06 21:23:38 -0300

    Update MatrixFastExp.cpp

--- code/Math/MatrixFastExp.cpp
+++ code/Math/MatrixFastExp.cpp
@@ -1,27 +1,27 @@
 struct Matrix{
-	long long mat[m][m];
-    
-	Matrix operator * (const Matrix &p){
-		Matrix ans;
-		for(int i = 0; i < m; ++i){
-			for(int j = 0; j < m; ++j){
-				for(int k = ans.mat[i][j] = 0; k < m; ++k){
-					ans.mat[i][j] = (ans.mat[i][j] + (mat[i][k] * p.mat[k][j]) % mod) % mod;
-				}
-			}
-		}
-		return ans;
-	}
+    long long mat[m][m];
+
+    Matrix operator * (const Matrix &p){
+    Matrix ans;
+    for(int i = 0; i < m; ++i){
+        for(int j = 0; j < m; ++j){
+            for(int k = ans.mat[i][j] = 0; k < m; ++k){
+                ans.mat[i][j] = (ans.mat[i][j] + (mat[i][k] * p.mat[k][j]) % mod) % mod;
+            }
+        }
+    }
+    return ans;
+}
 };
 
 Matrix fExp(Matrix a, long long b){
-	Matrix ans;
-	for(int i = 0; i < m; ++i) for(int j = 0; j < m; ++j) ans.mat[i][j] = (i == j);
+    Matrix ans;
+    for(int i = 0; i < m; ++i) for(int j = 0; j < m; ++j) ans.mat[i][j] = (i == j);
 
-	while(b){
-		if(b & 1) ans = ans * a;
-		a = a*a;
-		b >>= 1;
-	}
-	return ans;
+    while(b){
+        if(b & 1) ans = ans * a;
+        a = a*a;
+        b >>= 1;
+    }
+    return ans;
 }
