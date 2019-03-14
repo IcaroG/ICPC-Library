ef57df078ab603d740976f4012b47352e553a674
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-03-13 22:50:03 -0300

    Update Prime.cpp

--- code/Math/Prime.cpp
+++ code/Math/Prime.cpp
@@ -13,6 +13,22 @@ void sieve(){
     }
 }
 
+//O(nloglogn)
+void sieve(){
+	prime.push_back(2);
+	for(int i = 4; i<ms; i += 2){
+		notPrime[i] = true;
+	}
+	for(ll i = 3; i<ms; i += 2){
+		if(!notPrime[i]){
+			prime.push_back(i);
+			for(ll j = i*i; j < ms; j += i+i){
+				notPrime[j] = true;
+			}
+		}
+	}
+}
+
 vector<int> fact(int x){
     vector<int> ans;
     int idx = 0, pf = prime[idx];
