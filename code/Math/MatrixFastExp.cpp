9ef0207a0324d73e030981315598ceb819dd6e4d
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-04-09 11:50:44 -0300

    Update MatrixFastExp.cpp

--- code/Math/MatrixFastExp.cpp
+++ code/Math/MatrixFastExp.cpp
@@ -2,16 +2,16 @@ struct Matrix{
     long long mat[m][m];
 
     Matrix operator * (const Matrix &p){
-    Matrix ans;
-    for(int i = 0; i < m; ++i){
-        for(int j = 0; j < m; ++j){
-            for(int k = ans.mat[i][j] = 0; k < m; ++k){
-                ans.mat[i][j] = (ans.mat[i][j] + (mat[i][k] * p.mat[k][j]) % mod) % mod;
+        Matrix ans;
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < m; ++j){
+                for(int k = ans.mat[i][j] = 0; k < m; ++k){
+                    ans.mat[i][j] = (ans.mat[i][j] + (mat[i][k] * p.mat[k][j]) % mod) % mod;
+                }
             }
         }
+        return ans;
     }
-    return ans;
-}
 };
 
 Matrix fExp(Matrix a, long long b){
