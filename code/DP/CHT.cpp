6e93b1af4e1deb0eaa326e872503edceea19f275
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-04-12 17:44:23 -0300

    Update CHT.cpp

--- code/DP/CHT.cpp
+++ code/DP/CHT.cpp
@@ -1,3 +1,5 @@
+typedef long double ldouble_t;
+typedef long long ll;
 
 class HullDynamic {
 public:
@@ -21,7 +23,7 @@ public:
         }
 
         bool operator< (const Line& l) const {
-            if (is_query == 0) return m > l.m; // > min    < max
+            if (is_query == 0) return m < l.m; //     > min    < max
             return (start < l.start);
         }
     };
@@ -90,7 +92,7 @@ public:
         hull.insert(f);
     }
 
-    void insert_line(ll m, ll b) {
+    void add(ll m, ll b) {
         Line f(m, b, -inf, 0);
         iterator_t it = hull.lower_bound(f);
         if (it != hull.end() && it->m == f.m) {
@@ -111,7 +113,7 @@ public:
         update_right(it);
     }
 
-    ll get(ll x) {
+    ll query(ll x) {
         Line f(0, 0, x, 1);
         iterator_t it = hull.upper_bound(f);
         assert(it != hull.begin());
@@ -121,4 +123,44 @@ public:
 
 private:
     set<Line> hull;
+};
+
+//mais rapido
+
+bool Q;
+struct Line {
+    mutable ll k, m, p;
+    bool operator<(const Line& o) const {
+        return Q ? p < o.p : k < o.k;
+    }
+};
+
+struct HullDynamic : multiset<Line> {
+  // (for doubles, use inf = 1/.0, div(a,b) = a/b)
+    const ll inf = LLONG_MAX;
+
+    ll div(ll a, ll b) { // floored division
+        return a / b - ((a ^ b) < 0 && a % b); 
+    }
+
+    bool isect(iterator x, iterator y) {
+        if (y == end()) { x->p = inf; return false; }
+        if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+        else x->p = div(y->m - x->m, x->k - y->k);
+        return x->p >= y->p;
+    }
+    
+    void add(ll k, ll m) { // para min multiplicar por -1
+        auto z = insert({k, m, 0}), y = z++, x = y;
+        while (isect(y, z)) z = erase(z);
+        if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p)
+            isect(x, erase(y));
+    }
+
+    ll query(ll x) {
+        assert(!empty());
+        Q = 1; auto l = *lower_bound({0,0,x}); Q = 0;
+        return l.k * x + l.m; // para min multiplicar por -1
+    }
 };
