4d74f3a3be1907e116102cf8af356d2d8c736827
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-06-02 12:59:10 -0300

    Update Geometry.cpp

--- code/Geometry/Geometry.cpp
+++ code/Geometry/Geometry.cpp
@@ -2,20 +2,20 @@ const double inf = 1e100, eps = 1e-9;
 const double PI = acos(-1.0L);
 
 struct PT {
-	double x, y;
-	PT(double x = 0, double y = 0) : x(x), y(y) {}
-	PT operator + (const PT &p) const { return PT(x+p.x, y+p.y); }
-	PT operator - (const PT &p) const { return PT(x-p.x, y-p.y); }
-	PT operator * (double c) const { return PT(x*c, y*c); }
-	PT operator / (double c) const { return PT(x/c, y/c); }
+    double x, y;
+    PT(double x = 0, double y = 0) : x(x), y(y) {}
+    PT operator + (const PT &p) const { return PT(x+p.x, y+p.y); }
+    PT operator - (const PT &p) const { return PT(x-p.x, y-p.y); }
+    PT operator * (double c) const { return PT(x*c, y*c); }
+    PT operator / (double c) const { return PT(x/c, y/c); }
 
-	bool operator <(const PT &p) const {
-		if(abs(x - p.x) >= eps) return x < p.x;
-		return abs(y - p.y) >= eps && y < p.y;
-	}
-	bool operator ==(const PT &p) const {
-		return !(*this < p || p < *this);
-	}
+    bool operator <(const PT &p) const {
+        if(abs(x - p.x) >= eps) return x < p.x;
+        return abs(y - p.y) >= eps && y < p.y;
+    }
+    bool operator ==(const PT &p) const {
+        return !(*this < p || p < *this);
+    }
 };
 
 int cmp (double a, double b = 0) {
@@ -52,17 +52,17 @@ PT projectPointLine (PT a, PT b, PT c) {
 }
 
 PT reflectPointLine (PT a, PT b, PT c) {
-	PT p = projectPointLine(a, b, c);
-	return p*2 - c;
+    PT p = projectPointLine(a, b, c);
+    return p*2 - c;
 }
 
 PT projectPointSegment (PT a, PT b, PT c) {
-	double r = dot(b-a, b-a);
-	if (cmp(r) == 0) return a;
-	r = dot(b-a, c-a)/r;
-	if (cmp(r, 0) == 0) return a;
-	if (cmp(r, 1) == 0) return b;
-	return a + (b - a) * r;
+    double r = dot(b-a, b-a);
+    if (cmp(r) == 0) return a;
+    r = dot(b-a, c-a)/r;
+    if (cmp(r, 0) == 0) return a;
+    if (cmp(r, 1) == 0) return b;
+    return a + (b - a) * r;
 }
 
 double distancePointSegment (PT a, PT b, PT c) {
@@ -77,55 +77,55 @@ bool ptInSegment (PT a, PT b, PT c) {
 }
 
 bool parallel (PT a, PT b, PT c, PT d) {
-	return cmp(cross(b - a, c - d)) == 0;
+    return cmp(cross(b - a, c - d)) == 0;
 }
 
 bool collinear (PT a, PT b, PT c, PT d) {
-	return parallel(a, b, c, d) && cmp(cross(a - b, a - c)) == 0 && cmp(cross(c - d, c - a)) == 0;
+    return parallel(a, b, c, d) && cmp(cross(a - b, a - c)) == 0 && cmp(cross(c - d, c - a)) == 0;
 }
 
 bool segmentsIntersect (PT a, PT b, PT c, PT d) {
-	if (collinear(a, b, c, d)) {
-		if (cmp(dist(a, c)) == 0 || cmp(dist(a, d)) == 0 || cmp(dist(b, c)) == 0 || cmp(dist(b, d)) == 0) return true;
-		if (cmp(dot(c - a, c - b)) > 0 && cmp(dot(d - a, d - b)) > 0 && cmp(dot(c - b, d - b)) > 0) return false;
-		return true;
-	}
-	if (cmp(cross(d - a, b - a) * cross(c - a, b - a)) > 0) return false;
-	if (cmp(cross(a - c, d - c) * cross(b - c, d - c)) > 0) return false;
-	return true;
+    if (collinear(a, b, c, d)) {
+        if (cmp(dist(a, c)) == 0 || cmp(dist(a, d)) == 0 || cmp(dist(b, c)) == 0 || cmp(dist(b, d)) == 0) return true;
+        if (cmp(dot(c - a, c - b)) > 0 && cmp(dot(d - a, d - b)) > 0 && cmp(dot(c - b, d - b)) > 0) return false;
+        return true;
+    }
+    if (cmp(cross(d - a, b - a) * cross(c - a, b - a)) > 0) return false;
+    if (cmp(cross(a - c, d - c) * cross(b - c, d - c)) > 0) return false;
+    return true;
 }
 
 // r = a1 + t*d1, (r - a2) x d2 = 0
 PT computeLineIntersection (PT a, PT b, PT c, PT d) {
-	b = b - a; d = c - d; c = c - a;
-	assert(cmp(cross(b, d)) != 0); // checar sse sao paralelos
-	return a + b * cross(c, d) / cross(b, d);
+    b = b - a; d = c - d; c = c - a;
+    assert(cmp(cross(b, d)) != 0); // checar sse sao paralelos
+    return a + b * cross(c, d) / cross(b, d);
 }
 
 // Determina se o ponto p esta dentro do triangulo (a, b, c)
 bool ptInsideTriangle(PT p, PT a, PT b, PT c) {
-	if(cross(b-a, c-b) < 0) swap(a, b);
-	ll x = cross(b-a, p-b);
-	ll y = cross(c-b, p-c);
-	ll z = cross(a-c, p-a);
-	if(x > 0 && y > 0 && z > 0) return true;
-	if(!x) return ptInSegment(a,b,p);
-	if(!y) return ptInSegment(b,c,p);
-	if(!z) return ptInSegment(c,a,p);
-	return false;
+    if(cross(b-a, c-b) < 0) swap(a, b);
+    ll x = cross(b-a, p-b);
+    ll y = cross(c-b, p-c);
+    ll z = cross(a-c, p-a);
+    if(x > 0 && y > 0 && z > 0) return true;
+    if(!x) return ptInSegment(a,b,p);
+    if(!y) return ptInSegment(b,c,p);
+    if(!z) return ptInSegment(c,a,p);
+    return false;
 }
 
 // Determina se o ponto esta num poligono convexo em O(lgn)
 bool pointInConvexPolygon(const vector<PT> &p, PT q) {
-	PT pivot = p[0];
-	int x = 1, y = p.size();
-	while(y-x != 1) {
-		int z = (x+y)/2;
-		PT diagonal = pivot - p[z];
-		if(cross(p[x] - pivot, q - pivot) * cross(q-pivot, p[z] - pivot) >= 0) y = z;
-		else x = z;
-	}
-	return ptInsideTriangle(q, p[x], p[y], pivot);
+    PT pivot = p[0];
+    int x = 1, y = p.size();
+    while(y-x != 1) {
+        int z = (x+y)/2;
+        PT diagonal = pivot - p[z];
+        if(cross(p[x] - pivot, q - pivot) * cross(q-pivot, p[z] - pivot) >= 0) y = z;
+        else x = z;
+    }
+    return ptInsideTriangle(q, p[x], p[y], pivot);
 }
 
 // Determina se o ponto esta num poligono possivelmente nao-convexo
@@ -134,45 +134,45 @@ bool pointInConvexPolygon(const vector<PT> &p, PT q) {
 // Eh possivel converter num teste exato usando inteiros e tomando cuidado com a divisao
 // e entao usar testes exatos para checar se esta na borda do poligno
 bool pointInPolygon(const vector<PT> &p, PT q) {
-	bool c = 0;
-	for(int i = 0; i < p.size(); i++){
-		int j = (i + 1) % p.size();
-		if((p[i].y <= q.y && q.y < p[j].y || p[j].y <= q.y && q.y < p[i].y) &&
-			q.x < p[i].x + (p[j].x - p[i].x) * (q.y - p[i].y) / (p[j].y - p[i].y))
-			c = !c;
-	}
-	return c;
+    bool c = 0;
+    for(int i = 0; i < p.size(); i++){
+        int j = (i + 1) % p.size();
+        if((p[i].y <= q.y && q.y < p[j].y || p[j].y <= q.y && q.y < p[i].y) &&
+            q.x < p[i].x + (p[j].x - p[i].x) * (q.y - p[i].y) / (p[j].y - p[i].y))
+            c = !c;
+    }
+    return c;
 }
 
 // Determina se o ponto esta na borda do poligno
 bool pointOnPolygon(const vector<PT> &p, PT q) {
-	for(int i = 0; i < p.size(); i++)
-		if(dist2(projectPointSegment(p[i], p[(i + 1) % p.size()], q), q) < eps)
-			return true;
-	return false;
+    for(int i = 0; i < p.size(); i++)
+        if(dist2(projectPointSegment(p[i], p[(i + 1) % p.size()], q), q) < eps)
+            return true;
+    return false;
 }
 
 //circunferencia com tres pontos
 PT computeCircleCenter (PT a, PT b, PT c) {
-	b = (a + b) / 2; // bissector
-	c = (a + c) / 2; // bissector
-	return computeLineIntersection(b, b + rotateCW90(a - b), c, c + rotateCW90(a - c));
+    b = (a + b) / 2; // bissector
+    c = (a + c) / 2; // bissector
+    return computeLineIntersection(b, b + rotateCW90(a - b), c, c + rotateCW90(a - c));
 }
 
 //circunferencia com dois pontos e o raio, ate duas
 vector<PT> circle2PtsRad (PT p1, PT p2, double r) {
-	vector<PT> ret;
-	double d2 = dist2(p1, p2);
-	double det = r * r / d2 - 0.25;
-	if (det < 0.0) return ret;
-	double h = sqrt(det);
-	for (int i = 0; i < 2; i++) {
-		double x = (p1.x + p2.x) * 0.5 + (p1.y - p2.y) * h;
-		double y = (p1.y + p2.y) * 0.5 + (p2.x - p1.x) * h;
-		ret.push_back(PT(x, y));
-		swap(p1, p2);
-	}
-	return ret;
+    vector<PT> ret;
+    double d2 = dist2(p1, p2);
+    double det = r * r / d2 - 0.25;
+    if (det < 0.0) return ret;
+    double h = sqrt(det);
+    for (int i = 0; i < 2; i++) {
+        double x = (p1.x + p2.x) * 0.5 + (p1.y - p2.y) * h;
+        double y = (p1.y + p2.y) * 0.5 + (p2.x - p1.x) * h;
+        ret.push_back(PT(x, y));
+        swap(p1, p2);
+    }
+    return ret;
 }
 
 bool circleLineIntersection(PT a, PT b, PT c, double r) {
@@ -180,29 +180,29 @@ bool circleLineIntersection(PT a, PT b, PT c, double r) {
 }
 
 vector<PT> circleLine (PT a, PT b, PT c, double r) {
-	vector<PT> ret;
-	PT p = projectPointLine(a, b, c), p1;
-	double h = norm(c-p);
-	if (cmp(h,r) == 0) {
-		ret.push_back(p);
-	} else if (cmp(h,r) < 0) {
-		double k = sqrt(r*r - h*h);
-		p1 = p + (b-a)/(norm(b-a))*k;
-		ret.push_back(p1);
-		p1 = p - (b-a)/(norm(b-a))*k;
-		ret.push_back(p1);
-	}
-	return ret;
+    vector<PT> ret;
+    PT p = projectPointLine(a, b, c), p1;
+    double h = norm(c-p);
+    if (cmp(h,r) == 0) {
+        ret.push_back(p);
+    } else if (cmp(h,r) < 0) {
+        double k = sqrt(r*r - h*h);
+        p1 = p + (b-a)/(norm(b-a))*k;
+        ret.push_back(p1);
+        p1 = p - (b-a)/(norm(b-a))*k;
+        ret.push_back(p1);
+    }
+    return ret;
 }
 
 
 double computeSignedArea (const vector<PT> &p) {
-	double area = 0;
-	for (int i = 0; i < p.size(); i++) {
-		int j = (i+1) % p.size();
-		area += p[i].x*p[j].y - p[j].x*p[i].y;
-	}
-	return area;
+    double area = 0;
+    for (int i = 0; i < p.size(); i++) {
+        int j = (i+1) % p.size();
+        area += p[i].x*p[j].y - p[j].x*p[i].y;
+    }
+    return area;
 }
 
 double computeArea (const vector<PT> &p) {
