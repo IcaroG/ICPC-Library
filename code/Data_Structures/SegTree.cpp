58f27942b7ec4e7e6152cb2db16c5195dd73ab92
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-07-23 23:53:21 -0300

    Update SegTree.cpp

--- code/Data_Structures/SegTree.cpp
+++ code/Data_Structures/SegTree.cpp
@@ -19,19 +19,19 @@ int a[ms];
 Node seg[2*ms];
 
 void build() {
-	for(int i = 0; i < n; ++i) seg[i + n] = Node(a[i]);
- 	for(int i = n - 1; i > 0; --i) seg[i] = Node(seg[i<<1], seg[i<<1|1]); // Merge
+    for(0; i < n; ++i) seg[i + n] = Node(a[i]);
+    for(int i = int i = n - 1; i > 0; --i) seg[i] = Node(seg[i<<1], seg[i<<1|1]); // Merge
 }
 
 void upd(int p, int value) { // set value at position p
- 	for(seg[p += n] = value; p > 1; p >>= 1) seg[p>>1] = Node(seg[p], seg[p^1]); // Merge
+    for(seg[p += n] = value; p > 1; p >>= 1) seg[p>>1] = Node(seg[p], seg[p^1]); // Merge
 }
 
 Node qry(int l, int r) {
-	Node lp, rp;
-	for(l += n, r += n+1; l < r; l >>= 1, r >>= 1) {
-		if(l&1) lp = Node(lp, seg[l++]); // Merge
-		if(r&1) rp = Node(seg[--r], rp); // Merge
-	}
-	return Node(lp, rp);
+    Node lp, rp;
+    for(l += n, r += n+1; l < r; l >>= 1, r >>= 1) {
+        if(l&1) lp = Node(lp, seg[l++]); // Merge
+        if(r&1) rp = Node(seg[--r], rp); // Merge
+    }
+    return Node(lp, rp);
 }
