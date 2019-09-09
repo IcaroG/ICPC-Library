c1deb9d4c1ff6d8a040350a54aacc0fbee5d079b
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-09-09 01:05:14 -0300

    Create SOSDP.cpp

---
+++ code/DP/SOSDP.cpp
@@ -0,0 +1,15 @@
+// F[i] = Sum of all A[j] where j is a submask of i
+for(int i = 0; i<(1<<N); ++i)
+  F[i] = A[i];
+for(int i = 0;i < N; ++i){
+    for(int mask = 0; mask < (1<<N); ++mask){
+        if(mask & (1<<i))
+            F[mask] += F[mask^(1<<i)];
+    }
+}
+
+// Submasks
+for (int s=m; ; s=(s-1)&m) {
+  
+    if (s==0)  break;
+}
