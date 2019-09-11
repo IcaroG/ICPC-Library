15d80369ad50c18bc9c0a9b905962d170de00c1e
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2019-09-11 13:43:18 -0300

    Update Aho-Corasick.cpp

--- code/String/Aho-Corasick.cpp
+++ code/String/Aho-Corasick.cpp
@@ -1,109 +1,61 @@
-#include <bits/stdc++.h>
+template<const int ALPHA = 26, class T = string, const int off = 'a'>
+struct Aho {
+    struct Node {
+        int to[ALPHA];
+        int size;
+        int fail, pfail;
+        bool present;
 
-using namespace std;
+        Node() {
+            for(int i = 0; i < ALPHA; i++) {
+                to[i] = 0;
+            }
+            size = 0;
+            pfail = fail = 0;
+            present = false;
+            // maybe initialize some other stuff here
+        }
 
-const int MAXN = 1e6 + 7;
-const int sigma = 55;
+        // maybe add some other stuff here
+    };
 
-int fail[MAXN];
-vector<int> terminal[MAXN];
-vector<int> all;
-queue<int> q;
-int trie[MAXN][sigma], z;
-bool res[1010];
-
-void init() {
-  memset(trie[0], -1, sizeof trie[0]);
-  z = 1;
-}
-
-int get_id(char c) {
-	if (c >= 'a' && c <= 'z') return c - 'a';
-	return 26 + (c - 'A');
-}
-
-void insert(string &p, int idx) {
-  int cur = 0;
-  for(int i = 0; i < p.size(); i++) {
-    int id = get_id(p[i]);
-    if(trie[cur][id] == -1) {
-      memset(trie[z], -1, sizeof trie[z]);
-      trie[cur][id] = z++;
+    Aho() {
+        nodes.push_back(Node());
     }
-    cur = trie[cur][id];
-  }
-  all.push_back(cur);
-  terminal[cur].push_back(idx);
-}
 
-vector<int> merge(vector<int> &a, vector<int> &b) {
-	vector<int> ans(int(a.size()) + int(b.size()));
-	merge(a.begin(), a.end(), b.begin(), b.end(), ans.begin());
-	ans.resize(unique(ans.begin(), ans.end()) - ans.begin());
-	return ans;
-}
-
-void buildFailure() {
-  q.push(0);
-  while(!q.empty()) {
-    int node = q.front();
-    q.pop();
-    for(int pos = 0; pos < sigma; pos++) {
-      int &v = trie[node][pos];
-      int f = node == 0 ? 0 : trie[fail[node]][pos];
-      if(v == -1) {
-        v = f;
-      } else {
-        fail[v] = f;
-        q.push(v);
-        //terminal[v] += terminal[f];
-        terminal[v] = merge(terminal[v], terminal[f]);
-      }
+    int addString(const T &str) {
+        int on = 0;
+        for(auto ch : str) {
+            if(nodes[on].to[ch-off] == 0) {
+                nodes[on].to[ch-off] = (int) nodes.size();
+                nodes.push_back(Node());
+                nodes.back().size = 1 + nodes[on].size;
+            }
+            on = nodes[on].to[ch-off];
+        }
+        // makes this node present
+        nodes[on].present = true;
+        return on;
     }
-  }
-}
 
-void search(string &txt) {
-	int node = 0;
-	vector<int> ans;
-	for(int i = 0; i < txt.length(); i++) {
-		int pos = get_id(txt[i]);
-		node = trie[node][pos];
-		ans = merge(ans, terminal[node]);
-		//cout << "Na posicao " << i << '\n';
-		//for (auto v : terminal[node]) cout << v << ' ';
-		//cout << '\n';
-	}
-	for (auto v : ans) {
-		res[v] = true;
-	}
-}
-
-int main() {
-	int t;
-	cin >> t;
-	while (t--) {
-		init();
-		string s;
-		cin >> s;
-		int n;
-		cin >> n;
-		for (int i = 0; i < n; i++) {
-			string str;
-			cin >> str;
-			insert(str, i);
-		}
-		buildFailure();
-		memset(res, false, sizeof res);
-		search(s);
-		for (auto v : all) {
-			terminal[v].clear();
-		}
-		all.clear();
-		for (int i = 0; i < n; i++) {
-			if (res[i]) cout << "y\n";
-			else cout << "n\n";
-		}
-	}
-	return 0;
-}
+    void build() {
+        queue<int> que;
+        que.push(0);
+        while(!que.empty()) {
+            int on = que.front();
+            que.pop();
+            nodes[on].pfail = nodes[nodes[on].fail].present ? nodes[on].fail : nodes[nodes[on].fail].pfail;
+            // do stuff that carries over with fail here
+            for(int i = 0; i < ALPHA; i++) {
+                int &to = nodes[on].to[i];
+                if(to) {
+                    nodes[to].fail = on == 0 ? 0 : nodes[nodes[on].fail].to[i];
+                    que.push(to);
+                } else {
+                    to = nodes[nodes[on].fail].to[i];
+                }
+            }
+        }
+    }
+    vector<Node> nodes;
+};
