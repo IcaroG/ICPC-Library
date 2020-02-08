fa108f4e3af51bf62825592247f951146a61348e
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2020-02-08 14:52:06 -0300

    Update CutPolygon.cpp

--- code/Geometry/CutPolygon.cpp
+++ code/Geometry/CutPolygon.cpp
@@ -1,10 +1,11 @@
-vector<PT> cutPolygon (vector<PT> Q, PT a, PT b) {
+vector<PT> cutPolygon (vector<PT> Q, PT a, PT dir) { // reta ponto a vetor direcao dir
+    PT b = dir + a;
     PT vec = normalize(rotateCW90((b-a)));
     vector<PT> resp;
     for(int i=0; i<Q.size(); i++){
         int j = (i+1)%Q.size();
-        double n1 = (Q[i]-a)*(vec);
-        double n2 = (Q[j]-a)*(vec);
+        double n1 = dot((Q[i]-a), (vec));
+        double n2 = dot((Q[j]-a), (vec));
         if(n1>=-eps) resp.push_back(Q[i]);
         if((n1<-eps && n2>eps) || (n1>eps && n2<-eps)){
             resp.push_back(computeLineIntersection(a, b, Q[i], Q[j]));
