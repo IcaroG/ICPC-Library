e4d32ffe8180fd5a987b7faef78d7c0540bffee2
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2020-12-17 19:21:41 -0300

    Update PersistentSegTree.cpp

--- code/Data_Structures/PersistentSegTree.cpp
+++ code/Data_Structures/PersistentSegTree.cpp
@@ -5,24 +5,25 @@ struct Node{
 	Node *l = this, *r = this;
 };
 
+int n;
 int CNT = 1;
 Node buffer[ms * 20];
  
-Node* update(Node *root, int l, int r, int idx, int val){
+Node* update(Node *root, int idx, int val, int l = 0, int r = n){
 	Node *node = &buffer[CNT++];
 	*node = *root;
 	int mid = (l + r) / 2;
 	node->v += val;
 	if(l+1 != r){
-		if(idx < mid) node->l = update(root->l, l, mid, idx, val);
-		else node->r = update(root->r, mid, r, idx, val);
+		if(idx < mid) node->l = update(root->l, idx, val, l, mid);
+		else node->r = update(root->r, idx, val, mid, r);
 	}
 	return node;
 }
  
-int query(Node *node, int tl, int tr, int l, int r){
+int query(Node *node, int l, int r, int tl = 0, int tr = n){
 	if(l <= tl && tr <= r) return node->v;
 	if(tr <= l || tl >= r) return 0;
 	int mid = (tl+tr) / 2;
-	return query(node->l, tl, mid, l, r) + query(node->r, mid, tr, l, r);
+	return query(node->l, l, r, tl, mid) + query(node->r, l, r, mid, tr);
 }
